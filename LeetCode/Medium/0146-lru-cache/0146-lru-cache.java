38d93aeb6b035b3668ff64609dc8d0bff594274a
Yeji Im <yejiim.dev@gmail.com>
2024-05-23 15:17:55 +0900

    Time: 44 ms (77.56%), Space: 112.7 MB (71.98%) - LeetHub

---
+++ LeetCode/Medium/0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,85 @@
+class LRUCache {
+    Map<Integer, Node> cache;
+    Node head, tail;
+    int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new HashMap<>(capacity);
+        this.head = new Node(0,0);
+        this.tail = new Node(0,0);
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    // O(1)
+    public int get(int key) {
+        if (!cache.containsKey(key)) {
+            return -1;
+        }
+        Node node = cache.get(key);
+        moveToHead(node);
+        return node.value;
+    }
+
+    // O(1)
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            node.value = value;
+            moveToHead(node);
+        } else {
+            Node newNode = new Node(key, value);
+            cache.put(key, newNode);
+            addNode(newNode);
+            if (cache.size() > capacity) {
+                Node tail = popTail();
+                cache.remove(tail.key);
+            }
+        }     
+    }
+
+    private void addNode(Node node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(Node node) {
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    private void moveToHead(Node node) {
+        removeNode(node);
+        addNode(node);
+    }
+
+    private Node popTail() {
+        Node res = tail.prev;
+        removeNode(res);
+        return res;
+    }
+
+    private static class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
