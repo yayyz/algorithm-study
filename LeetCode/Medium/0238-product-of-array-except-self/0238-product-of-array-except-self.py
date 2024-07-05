81c709a7feadc5dd9cac974bb18e46b79263c789
Yeji Im <yejiim.dev@gmail.com>
2024-07-05 09:57:12 +0900

    Time: 263 ms (78.14%), Space: 25.7 MB (74.24%) - LeetHub

--- LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,30 +1,16 @@
 class Solution:
+    # prefix, postfix probelm :( 
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        output, prefix, postfix = [], [], []
-        size = len(nums)
-        # create prefix     
-        for i in range(size):
-            if i == 0:
-                prefix.append(nums[i])
-            else: 
-                prefix.append(prefix[i - 1] * nums[i])
-            
-        # create postfix 
-        for i in range(size - 1, -1, -1):
-            if i == size -1: 
-                postfix.insert(0, nums[i])
-            else: 
-                postfix.insert(0, postfix[0] * nums[i])
-        
-        # create output 
-        for i in range(size):
-            pre = i - 1
-            post = i + 1
-            if pre < 0:
-                output.append(postfix[post])
-            elif post >= size: 
-                output.append(prefix[pre])
-            else: 
-                output.append(prefix[pre] * postfix[post])
-            
+        output = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            output[i] = prefix 
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums) -1, -1, -1):
+            output[i] *= postfix
+            postfix *= nums[i]
+
         return output 
