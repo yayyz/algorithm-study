a04bcfeac6c8f01bc15251052bc1918c8bfd7872
Yeji Im <yejiim.dev@gmail.com>
2024-11-04 09:16:24 +0900

    Time: 3 ms (54.2%), Space: 17.9 MB (22.76%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -1,17 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_map = {}
-        ret = []
-
+        hashmap = {}
         for i, num in enumerate(nums):
-            num_map[num] = i
-
-        print(num_map)
-        for i, num in enumerate(nums):
-            tmp = target - num
-            if tmp in num_map and num_map[tmp] != i:
-                ret.append(i)
-                ret.append(num_map[tmp])
-                break
-
-        return ret
+            complement = target - num
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            hashmap[num] = i
+            
+    
