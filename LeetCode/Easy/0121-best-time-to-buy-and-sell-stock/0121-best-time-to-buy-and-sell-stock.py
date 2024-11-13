4732c4de6c649d5d4201010e1a5582126c23d762
Yeji Im <yejiim.dev@gmail.com>
2024-11-13 23:29:22 +0900

    Time: 89 ms (56.21%), Space: 25.8 MB (99.55%) - LeetHub

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # any point where the buying price is better, update 
-        minPrice = prices[0]
-        # return value 
+        buy, sell = 0, 1
         maxProfit = 0
-        
-        for i in range(1, len(prices)): 
-            price = prices[i]
-            if minPrice > price: # buy 
-                minPrice = price 
-            elif minPrice < price and (price - minPrice) > maxProfit: # sell 
-                maxProfit = price - minPrice
-        
+
+        while sell < len(prices):
+            if prices[buy] < prices[sell]:
+                maxProfit = max(maxProfit, prices[sell] - prices[buy])
+            else:
+                buy = sell 
+            sell += 1 
+
         return maxProfit
 
         
