d5d073b63de6931012095eadfc4e0f568621d410
Yeji Im <yejiim.dev@gmail.com>
2024-11-15 08:25:33 +0900

    Time: 6 ms (85.65%), Space: 17.4 MB (37.34%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,16 +1,15 @@
 class Solution:
-    def isPalindrome(self, s: str) -> bool:        
-        s = ''.join(filter(str.isalnum, s)).lower()
-        if not (s and s.strip()): return True 
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(char for char in s if char.isalnum()).lower()
         
         head = 0
-        tail = len(s) -1
-        while head < tail:
+        tail = len(s) - 1
+
+        while head < tail: 
             if s[head] != s[tail]:
                 return False
-            else: 
+            else:
                 head += 1
-                tail -= 1
+                tail -= 1 
         
-        return True
-        
+        return True 
