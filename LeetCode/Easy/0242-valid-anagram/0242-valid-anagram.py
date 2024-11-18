4c6774d3c705d48b3eaa6328d39762e8ffd4d4f6
Yeji Im <yejiim.dev@gmail.com>
2024-11-19 07:21:11 +0900

    Time: 15 ms (49.02%), Space: 16.7 MB (73.19%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): return False
-        return sorted(s) == sorted(t)
+        # create dict
+        map = {}
+        for char in s: 
+            if char in map: 
+                map[char] += 1
+            else:
+                map[char] = 1
+        
+        # compare with t 
+        for char in t:
+            if char in map: 
+                map[char] -= 1
+                if map[char] < 0:
+                    return False
+            else: 
+                return False
+        
+        return all(value == 0 for value in map.values())                
